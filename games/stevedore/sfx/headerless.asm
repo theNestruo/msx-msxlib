61d7def7ebd55a7093b22176380d598746878419
Néstor Sancho <theNestruo@users.noreply.github.com>
2017-11-11 12:17:56 +0100

    Clean-up

---
+++ games/stevedore/sfx/headerless.asm
@@ -0,0 +1,9 @@
+
+; Removes the first 100 bytes of the PT3 files
+
+	fname	"warehouse.pt3.hl"
+	incbin	"warehouse.pt3", 100
+
+	fname	"ship.pt3.hl"
+	incbin	"ship.pt3", 100
+	
--- wyzplayer-test.bat
+++
@@ -1,2 +0,0 @@
-tniasm games\wyzplayer-test\wyzplayer-test.asm games\wyzplayer-test\wyzplayer-test.rom
-start games\wyzplayer-test\wyzplayer-test.rom
