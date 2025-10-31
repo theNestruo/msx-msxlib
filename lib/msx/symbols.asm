5af84596151e217ae02615858d1bdc145d0968e1
Néstor Sancho <theNestruo@gmail.com>
2025-10-31 08:25:47 +0100

    More symbols

--- lib/msx/symbols.asm
+++ lib/msx/symbols.asm
@@ -247,10 +247,124 @@
 	PROCNM:	equ $fd89 ; Work area of the instructions CALL and OPEN. Contents the instruction name or device name (16b)
 
 ; MSX system hooks
-	HKEYI:	equ $fd9a ; Called at the beginning of the KEYINT interrupt routine
-	HTIMI:	equ $fd9f ; Called by the KEYINT interrupt routine when Vblank interrupt occurs
-	HRUNC:	equ $fecb ; Called at the beginning of the routine RUNC, used by the Basic instructions NEW and RUN
-	HSTKE:	equ $feda ; This hook allows to automatically re-execute the ROM after the disks are installed
+	HKEYI:		equ $fd9a ; Called at the beginning of the KEYINT interrupt routine
+	HTIMI:		equ $fd9f ; Called by the KEYINT interrupt routine when Vblank interrupt occurs
+	HCHPU:		equ $fda4 ; Called at the beginning of the routine CHPUT (Main-ROM at 00A2h) to display a character on the screen.
+	HDSPC:		equ $fda9 ; Called at the beginning of internal routine DSPCSR that displays the cursor.
+	HERAC:		equ $fdae ; Called at the beginning of internal routine ERACSR that erases the cursor.
+	HDSPF:		equ $fdb3 ; Called at the beginning of the routines DSPFNK (Main-ROM at 00CFh) that displays the contents of the function keys.
+	HERAF:		equ $fdb8 ; Called at the beginning of the routine ERAFNK (Main-ROM at 00CCh) to erase the text of fonction keys.
+	HTOTE:		equ $fdbd ; Called at the beginning of the routine TOTEXT (Main-ROM at 00D2h) to back into text mode.
+	HCHGE:		equ $fdc2 ; Called at the beginning of the routine CHGET (Main-ROM at 009Fh) to read a character on the keyboard.
+	HINIP:		equ $fdc7 ; Called at the beginning of internal routine INIPAT that initializes characters font in VRAM.
+	HKEYC:		equ $fdcc ; Called at the beginning of internal routine KEYCOD of keyboard reading.
+	HKYEA:		equ $fdd1 ; Called at the beginning of internal routine KYEASY that decodes the keyboard.
+	HNMI:		equ $fdd6 ; Called at the beginning of non-maskable interrupts routine (Main-ROM at 0066h).
+	HPINL:		equ $fddb ; Called at the beginning of Bios routine PINLIN (Main-ROM at 00AEh) that manages the input of a program line to the keyboard.
+	HQINL:		equ $fde0 ; Called at the beginning of Bios routine QINLIN (Main-ROM at 00B4h) that manages the line input with LINEINPUT instruction of Basic.
+	HINLI:		equ $fde5 ; Called at the beginning of Bios routine INLIN (Main-ROM at 000B1h) that store the entered characters until STOP or RET key is pressed.
+	HONGO:		equ $fdea ; Called at the beginning of the system routine that manages the Basic instructions like ON GOTO, ON GOSUB.
+	HDSKO:		equ $fdef ; Called at the beginning of Basic instruction DSKO$.
+	HSETS:		equ $fdf4 ; Called at the beginning of Basic instruction SET.
+	HNAME:		equ $fdf9 ; Called at the beginning of Basic instruction NAME.
+	HKILL:		equ $fdfe ; Called at the beginning of Basic instruction KILL.
+	HIPL:		equ $fe03 ; Called at the beginning of Basic instruction IPL.
+	HCOPY:		equ $fe08 ; Called at the beginning of Basic instruction COPY.
+	HCMD:		equ $fe0d ; Called at the beginning of Basic instruction CMD.
+	HDSKF:		equ $fe12 ; Called at the beginning of Basic instruction DSKF$.
+	HDSKI:		equ $fe17 ; Called at the beginning of Basic instruction DSKI$.
+	HATTR:		equ $fe1c ; Called at the beginning of Basic instruction ATTR$.
+	HLSET:		equ $fe21 ; Called at the beginning of Basic instruction LSET$.
+	HRSET:		equ $fe26 ; Called at the beginning of Basic instruction RSET$.
+	HFIEL:		equ $fe2b ; Called at the beginning of Basic instruction FIELD.
+	HMKI:		equ $fe30 ; Called at the beginning of Basic instruction MKI$.
+	HMKS:		equ $fe35 ; Called at the beginning of Basic instruction MKS$.
+	HMKD:		equ $fe3a ; Called at the beginning of Basic instruction MKD$.
+	HCVI:		equ $fe3f ; Called at the beginning of Basic instruction CVI$.
+	HCVS:		equ $fe44 ; Called at the beginning of Basic instruction CVS$.
+	HCVD:		equ $fe49 ; Called at the beginning of Basic instruction CVD.
+	HGETP:		equ $fe4e ; Called at the beginning of the routine GETPTR ("GET file PoinTeR"), positioning on a file.
+	HSETF:		equ $fe53 ; Called at the beginning of the routine SETFIL ("SET FILe"), position a pointer on a previously opened file.
+	HNOFO:		equ $fe58 ; Called at the beginning of the routine NOFOR ("NO FOR clause"), used by Basic instruction OPEN of Basic.
+	HNULO:		equ $fe5d ; Called at the beginning of the routine NULOPN ("NULl file OPen"), used by Basic instructions LOAD, KILL, MERGE, etc.
+	HNTFL:		equ $fe62 ; Called at the beginning of the routine NTFL0 ("NoT FiLe number 0"), used by Basic instructions LOAD, KILL, MERGE, etc.
+	HMERG:		equ $fe67 ; Called at the beginning of the MERGE ("MERGE program files") routine, used by Basic instructions MERGE and LOAD.
+	HSAVE:		equ $fe6c ; Called at the beginning of the routine SAVE, used by Basic instruction SAVE.
+	HBINS:		equ $fe71 ; Called at the beginning of the routine BINSAR ("BINary SAVe") used by the instruction SAVE of Basic.
+	HBINL:		equ $fe76 ; Called at the beginning of internal routine BINLOD ("BINary LOaD") used by the instruction LOAD of Basic.
+	HFILE:		equ $fe7b ; Called at the beginning of Basic instruction FILES.
+	HDGET:		equ $fe80 ; Called at the beginning of internal routine DGET, used by the basic instruction GET/PUT.
+	HFILO:		equ $fe85 ; Called at the beginning of internal routine FILOU1 of file output.
+	HINDS:		equ $fe8a ; Called at the beginning of the internal routine INDSKC of disk attribute input.
+	HRSLF:		equ $fe8f ; Called at the beginning of the internal routine of re-selection of previous drive.
+	HSAVD:		equ $fe94 ; Called at the beginning of the internal routine of store the current drive, used by the LOF, LOC, EOF, FPOS instructions, etc.
+	HLOC:		equ $fe99 ; Called at the beginning of the internal routine LOC, used by the LOC instruction of the Basic.
+	HLOF:		equ $fe9e ; Called at the beginning of the internal routine LOF, used by the LOF instruction of the Basic.
+	HEOF:		equ $fea3 ; Called at the beginning of the internal routine EOF, used by the EOF instruction of the Basic.
+	HFPOS:		equ $fea8 ; Called at the beginning of the internal routine FPOS, used by the FPOS instruction of the Basic.
+	HBAKU:		equ $fead ; Called at the beginning of the internal routine BAKUPT.
+	HPARD:		equ $feb2 ; Called at the beginning of the internal routine PARDEV that parses the device name.
+	HNODE:		equ $feb7 ; Called at the beginning of the internal routine NODEVN that is called when no name has been found in the device name table.
+	HPOSD:		equ $febc ; Called at the beginning of the internal routine POSDSK.
+	HDEVN:		equ $fec1 ; Called at the beginning of the internal routine DEVNAM to process the device name.
+	HGEND:		equ $fec6 ; Called at the beginning of the internal routine GENDSP to assign the device name.
+	HRUNC:		equ $fecb ; Called at the beginning of the routine RUNC, used by the Basic instructions NEW and RUN
+	HCLEA:		equ $fed0 ; Called at the beginning of the CLEARC routine that initializes the variables table, used by the CLEAR instruction of the Basic.
+	HLOPD:		equ $fed5 ; Called at the beginning of the internal routine LOPDFT, initialize the variable table, used by the CLEAR instruction of the Basic.
+	HSTKE:		equ $feda ; This hook allows to automatically re-execute the ROM after the disks are installed
+	HISFL:		equ $fedf ; Called at the beginning of the internal routine ISFLIO that tests whether the file to write or read.
+	HOUTD:		equ $fee4 ; Called at the beginning of the Bios routine OUTDO, output a screen character or printer.
+	HCRDO:		equ $fee9 ; Called at the beginning of the CRDO routine that sends a CR (0Dh) and a LF (code 0Ah).
+	HDSKC:		equ $feee ; Called at the beginning of the internal routine DSKCHI, for the entry of the attribute of a disk.
+	HDOGR:		equ $fef3 ; Called at the beginning of the internal routine DOGRPH, used by the instructions of the Basic of graphical tracing. (LINE, CIRCLE, ...)
+	HPRGE:		equ $fef8 ; Called at the end of Basic program execution.
+	HERRP:		equ $fefd ; Called at the beginning of the routine ERRPRT that displays the error message under Basic.
+	HERRF:		equ $ff02 ; Called at the end of the routine that displays the error message under Basic.
+	HREAD:		equ $ff07 ; Called at the beginning of the routine READY that displays the message "Ok" (or the one defined by the user on MSX2 or newer)
+	HMAIN:		equ $ff0c ; Called at the beginning of the MAIN routine, used at each access to the Basic interpreter.
+	HDIRD:		equ $ff11 ; Called at the beginning of the DIRDO routine which is called when executing instructions in direct mode.
+	HFINI:		equ $ff16 ; Called at the beginning of the routine FININT that initializes the interpretation of a basic instruction.
+	HFINE:		equ $ff1b ; Called at the end of the routine that initializes the interpretation of a basic instruction.
+	HCRUN:		equ $ff20 ; Called at the beginning of the routine CRUNCH that transforms a Basic line into keywords.
+	HCRUS:		equ $ff25 ; Called at the beginning of the CRUSH routine that searches for a keyword in the alphabetical list in Rom.
+	HISRE:		equ $ff2a ; Called at the beginning of the ISRESV routine when a keyword is found by the CRUSH routine.
+	HNTFN:		equ $ff2f ; Called at the beginning of the NTFN2 routine when a keyword is followed by a line number.
+	HNOTR:		equ $ff34 ; Called at the beginning of the NOTRSV routine when the sequence of characters examined by the CRUNCH routine is not a keyword.
+	HSNGF:		equ $ff39 ; Called at the beginning of basic instruction FOR.
+	HNEWS:		equ $ff3e ; Called at the end of process of a Basic instruction.
+	HGONE:		equ $ff43 ; Called at the beginning of the GONE2 routine, used by the jump instructions (GOTO, THEN, ...)
+	HCNRG:		equ $ff48 ; Called at the beginning of the CHRGET routine, enter a character on the keyboard.
+	HRETU:		equ $ff4d ; Called at the beginning of basic instruction RETURN.
+	HPRTF:		equ $ff52 ; Called at the beginning of basic instruction PRINT.
+	HCOMP:		equ $ff57 ; Called at the beginning of internal routine COMPRT of basic instruction PRINT.
+	HFINP:		equ $ff5c ; Called at the end of the text display under Basic.
+	HTRMN:		equ $ff61 ; Call: When an input error with basic instruction READ/INPUT.
+	HFRME:		equ $ff66 ; Called at the beginning of expression evaluator routine of Basic interpreter.
+	HNTPL:		equ $ff6b ; Called at the beginning of expression evaluator routine of Basic interpreter.
+	HEVAL:		equ $ff70 ; Called at the beginning of expression evaluator routine of Basic interpreter.
+	HOKNO:		equ $ff75 ; Called at the beginning of transcendental function routine of Basic interpreter.
+	HMDIN:		equ $ff75 ; Called at the beginning of interruptions routine of Midi interface input.
+	HFING:		equ $ff7a ; Called at the beginning of factor evaluator routine of Basic interpreter.
+	HISMI:		equ $ff7f ; Called at the beginning of basic instruction MID$.
+	HWIDT:		equ $ff84 ; Called at the beginning of basic instruction WIDTH.
+	HLIST:		equ $ff89 ; Called at the beginning of basic instruction LIST/LLIST.
+	HBUFL:		equ $ff8e ; Called when instructions LIST detokenise a basic instruction.
+	HFRQINT:	equ $ff93 ; Called at the beginning of frequency interrupt routine
+	HMDTM:		equ $ff93 ; Called at the beginning of the routine of Midi interface timer.
+	HSCNE:		equ $ff98 ; Called at the beginning of the routine SCNEX2 from Basic interpretor, conversion of a line number to a memory address and vice versa.
+	HFRET:		equ $ff9d ; Called at the beginning of the routine FRETMP from Basic interpretor, search for a free location to store the descriptor an alphanumeric variable.
+	HPTRG:		equ $ffa2 ; Called at the beginning of the routine PTRGET from Basic interpretor, to get the pointer to found a variable.
+	HPHYD:		equ $ffa7 ; Called at the beginning of the routine PHYDIO (Main-ROM at 0144h).
+	HFORM:		equ $ffac ; Called at the beginning of the routine FORMAT (Main-ROM at 0147h).
+	HERRO:		equ $ffb1 ; Called at the beginning of the error handler routine of Basic.
+	HLPTO:		equ $ffb6 ; Called at the beginning of the routine LPTOUT (Main-ROM at 00A5h).
+	HLPTS:		equ $ffbb ; Called at the beginning of the routine LPTSTT (Main-ROM at 00A5h).
+	HSCRE:		equ $ffc0 ; Called at the beginning of the instruction SCREEN of Basic.
+	HPLAY:		equ $ffc5 ; Called at the beginning of the instruction PLAY of Basic.
+	EXTBIO:		equ $ffca ; (This is not a hook)
+	HBGFD:		equ $ffcf ; Called before physical operation with disk. (MSX-DOS1 only)
+	HENFD:		equ $ffd4 ; Called after physical operation with disk. (MSX-DOS1 only)
+
 	HOOK_SIZE:	equ HTIMI - HKEYI ; (5b)
 
 ; MSX2 system variables
